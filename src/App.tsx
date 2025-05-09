f6ef6f7a63f50b0a9a2d0ec29fd0a5745742a6b2
BILON Ikram <bilonikram20@gmail.com>
2025-05-09 11:57:50 +0100

    remove base name

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
   console.log('🖥️ Rendering main application interface');
   return (
-    <Router basename={qiankunWindow.__POWERED_BY_QIANKUN__ ? '/repdashboard' : '/'}>
+    <Router>
       <div className="flex h-screen bg-gray-50">
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
