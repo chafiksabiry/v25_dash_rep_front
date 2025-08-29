18c1d345e23a86c0f1787a2c30560e39df7970cd
El Houcine Qara <elhoucine.qara@harx.ai>
2025-08-29 16:01:48 +0000

    Edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: 'https://rep-dashboard.harx.ai/',
+    base: 'https://preprod-rep-dashboard.harx.ai/',
     plugins: [
       react(),
       qiankun('repdashboard', {
