16dc4172fda8c06d1cfeb3c4aea13db7ffccfc67
elhoucineqara114 <elhoucine.qara@harx.ai>
2026-05-16 06:09:34 +0100

    Update

--- src/components/CallRecords.tsx
+++ src/components/CallRecords.tsx
@@ -23,8 +23,7 @@ import {
   TrendingUp,
   Activity as ActivityIcon,
   ChevronDown,
-  Clock,
-  BookOpen
+  Clock
 } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import api from '../utils/client';
